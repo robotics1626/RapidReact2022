4dd0c696a9154a47fc5bd23d7335127038690bfa
marsmuce <37277612+marsmuce@users.noreply.github.com>
2022-01-13 15:35:02 -0500

    Fix stuff

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,12 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    // Operator and Drive Controllers
+    public static final int OPERATOR = 0;
+    public static final int JOYSTICK_LEFT = 1;
+    public static final int JOYSTICK_RIGHT = 2; 
+
+    // Temp. Piston
+    public static final int [] TEMP_SOLENOID = new int[] {0, 1};
+}
