66edb5fc635efcddfc272de2edae351f8de22cf4
marsmuce <37277612+marsmuce@users.noreply.github.com>
2022-02-23 00:12:53 -0500

    Update robot

---
+++ src/main/java/frc/robot/commands/Gatekeeper/Girlboss.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Gatekeeper;
+
+import java.util.function.DoubleSupplier;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Gatekeeper;
+
+public class Girlboss extends CommandBase {
+    private final Gatekeeper m_gatekeeper;
+    private final DoubleSupplier m_input;
+
+    public Girlboss(Gatekeeper gatekeeper, DoubleSupplier input) {
+        m_gatekeeper = gatekeeper;
+        m_input = input;
+
+        addRequirements(m_gatekeeper);
+    }
+
+    @Override
+    public void initialize() {}
+
+    @Override
+    public void execute() {
+        m_gatekeeper.Girlboss(m_input.getAsDouble());
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        m_gatekeeper.stop();
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
