9ec8430fe2356cbe69b48bb77e680e835e7edb8e
mgarmao <mgarmao830@gmail.com>
2022-03-19 15:13:57 -0400

    fixing limelight pipelines

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,49 +1,50 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
 import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Limelight extends SubsystemBase {
-  
+  private NetworkTable table;
+  private String tableName;
   /** Creates a new Limelight. */
   public Limelight() {
-
+    tableName = "limelight";
+    table = NetworkTableInstance.getDefault().getTable(tableName);
   }
 
-  public double redballX(){
-    double redBallX = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
-    return redBallX;
-  }
-
-  public double redballY(){
-    double redBallY = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
-    return redBallY;
-  }
-
-  public double blueballX(){
-    double blueBallX = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
+  public double ballY(){
+    setPipeline(2);
+    table.getEntry("pipeline").setNumber(2);
+    NetworkTableEntry tx = table.getEntry("tx");
+    double blueBallX = tx.getDouble(0.0);
     return blueBallX;
   }
 
-  public double blueballY(){
-    double blueBallY = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
+  public double ballX(){
+    setPipeline(1);
+    NetworkTableEntry ty = table.getEntry("ty");
+    double blueBallY = ty.getDouble(0.0);
     return blueBallY;
+  }
+
+  public void setPipeline(Integer pipeline) {
+    NetworkTableEntry pipelineEntry = table.getEntry("pipeline");
+    pipelineEntry.setNumber(pipeline);
   }
 
   @Override
   public void periodic() {
+    table.getEntry("ledMode").setNumber(2);
+    setPipeline(1);
+    SmartDashboard.putNumber("Target Ball X:",ballX());
+    SmartDashboard.putNumber("Target Ball Y:",ballY());
 
-  SmartDashboard.putNumber("Red Ball X:",redballX());
-  SmartDashboard.putNumber("Red Ball Y:",redballY());
-
-  SmartDashboard.putNumber("Blue Ball X:",blueballX());
-  SmartDashboard.putNumber("Blue Ball Y:",blueballY())
-
+    setPipeline(2);
+    SmartDashboard.putNumber("Blue Ball X:",ballX());
+    SmartDashboard.putNumber("Blue Ball Y:",ballY());
   }
 }
+
