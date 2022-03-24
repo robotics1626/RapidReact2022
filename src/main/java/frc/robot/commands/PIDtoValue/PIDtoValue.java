f9f15866fa5565b42311e6615c066faba6d6f3f6
mgarmao <mgarmao830@gmail.com>
2022-03-24 17:18:11 -0400

    No Code Issue

---
+++ src/main/java/frc/robot/commands/PIDtoValue/PIDtoValue.java
@@ -0,0 +1,15 @@
+package frc.robot.commands.PIDtoValue;
+
+import java.util.function.DoubleSupplier;
+
+import frc.robot.subsystems.Limelight;
+
+public class PIDtoValue {
+    // public double changeToMotorValueLeft(Limelight limelight, DoubleSupplier PID) {
+    //     Limelight m_limelight = limelight;
+    //     DoubleSupplier m_PID = PID;
+    //     double motorLeft = 0;
+    //     m_PID
+    //     return motorLeft;
+    // }
+}
