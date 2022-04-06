565d569cd2af6706ff6d990f87b566dfd8bf3745
mgarmao <mgarmao830@gmail.com>
2022-04-05 20:09:50 -0400

    starting 4 ball auto

---
+++ src/main/java/frc/robot/commands/Autonomous/FourBallAuto.java
@@ -0,0 +1,79 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+// Autonomous program created with help from Team 303
+
+package frc.robot.commands.Autonomous;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+import static frc.robot.RobotContainer.*;
+
+import frc.robot.commands.Drivetrain.*;
+import frc.robot.commands.Gatekeeper.*;
+import frc.robot.commands.Indexer.*;
+import frc.robot.commands.Shooter.*;
+
+public class FourBallAuto extends SequentialCommandGroup {
+    public FourBallAuto() {
+      addCommands(
+        new InstantCommand(m_intake::extend, m_intake),
+        new TankDrive(m_drivetrain, () -> 0.65, () -> 0.65).alongWith(
+          new InstantCommand(m_intake::retrieve, m_intake),
+          new IndexerController(m_indexer, () -> -0.75),
+          new InstantCommand(m_gatekeeper::manual, m_gatekeeper)
+        ).withTimeout(1.5),
+        new InstantCommand(m_gatekeeper::stop, m_gatekeeper).alongWith(
+          new InstantCommand(m_intake::stop, m_intake)
+        ),
+        new InstantCommand(m_intake::stop, m_intake),
+        new InstantCommand(m_intake::retract, m_intake),
+        new TankDrive(m_drivetrain, () -> -0.65, () -> -0.65).withTimeout(1.5),
+        new Rotate(180).withTimeout(0.7),
+        new TankDrive(m_drivetrain, () -> 0.65, () -> 0.65).withTimeout(1.5),
+
+        new SequentialCommandGroup(
+        new ShooterController(m_shooter, () -> 1.0).withTimeout(5)).alongWith(
+          new GatekeeperController(m_gatekeeper, () -> 1.0).withTimeout(0.7),
+          new IndexerController(m_indexer, () -> -1.0).withTimeout(2)
+        ).withTimeout(0.7),
+
+        new ShooterController(m_shooter, () -> 1.0).withTimeout(5).alongWith(
+          new GatekeeperController(m_gatekeeper, () -> 1.0).withTimeout(0.7),
+          new IndexerController(m_indexer, () -> -1.0).withTimeout(2)
+        ).withTimeout(0.7),
+        
+        //Entering a land of imagination
+
+        //move back and turn around
+        new TankDrive(m_drivetrain, () -> -0.4, () -> -0.4).withTimeout(0.85),
+        new RotateCounterClock(35).withTimeout(0.5),
+        
+        //extend stuff and start driving
+        new TankDrive(m_drivetrain, () -> 0.4, () -> 0.4).alongWith(
+          new InstantCommand(m_intake::retrieve, m_intake),
+          new IndexerController(m_indexer, () -> -0.75),
+          new InstantCommand(m_gatekeeper::manual, m_gatekeeper)
+        ).withTimeout(1),
+
+        //slow down
+        new TankDrive(m_drivetrain, () -> 0.4, () -> 0.4).withTimeout(0.5),
+
+        //stop for a little
+        new TankDrive(m_drivetrain, () -> 0.0, () -> 0.0).withTimeout(0.2),
+        
+        //move back and turn around 
+        new TankDrive(m_drivetrain, () -> -0.4, () -> -0.4).alongWith(
+            new InstantCommand(m_intake::retract, m_intake),
+            new InstantCommand(m_gatekeeper::stop, m_gatekeeper)
+        ).withTimeout(1.5),
+
+        new Rotate(215).withTimeout(3),
+
+        //move back to centerpoint
+        new TankDrive(m_drivetrain, () -> 0.4, () -> 0.4).withTimeout(2)
+      );
+  }
+}
--- src/main/java/frc/robot/commands/Autonomous/ThreeBall.java
+++
@@ -1,5 +0,0 @@
-package frc.robot.commands.Autonomous;
-
-public class ThreeBall {
-    
-}
